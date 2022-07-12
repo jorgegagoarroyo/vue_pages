3a9f28d837e7e085c37ee4f7bc4414a105cbbdac
jorge <gagoarroyojorge@fpllefia.com>
2022-07-12 10:30:57 +0200

    primer commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div v-for="persona in arrayPersonas" :key="persona.id">
+      <p> persona {{persona.nombre}} {{persona.apellido}}</p>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      arrayPersonas: [
+        {
+          nombre: 'pepe',
+          apellido: 'gote'
+        },
+        {
+          nombre: 'raul',
+          apellido: 'traderecha'
+        }
+      ]
+    }
+  }
+}
+</script>
